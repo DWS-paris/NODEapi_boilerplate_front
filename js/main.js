92b65515aeb728fdf6d2b4e5f472d0d4bcd61794
Julien Noyer <j.noyer@digitalworkshop.fr>
2020-06-11 12:21:32 +0200

    Ajout du login et du check user

--- js/main.js
+++ js/main.js
@@ -18,16 +18,69 @@ const register = (formTag, name, email, password) => {
     
 }
 
+const login = (formTag, email, password) => {
+    formTag.addEventListener('submit',  event => {
+        event.preventDefault();
+
+        new FETCHrequest(
+            'http://localhost:6985/api/auth/register', 
+            'POST', 
+            { email, password }
+        )
+        .sendRequest()
+        .then( apiResponse => {
+            console.log(apiResponse)
+            localStorage.setItem('email', email)
+            localStorage.setItem('password', password)
+        })
+        .catch( apiResponse => {
+            console.log(apiResponse)
+        })
+    })
+}
+
+const checkUser = () => {
+    if( localStorage.getItem('email') && localStorage.getItem('password') ){
+        new FETCHrequest(
+            'http://localhost:6985/api/auth/register', 
+            'POST', 
+            { email: localStorage.getItem('email'), password: localStorage.getItem('password') }
+        )
+        .sendRequest()
+        .then( apiResponse => {
+            console.log(apiResponse)
+            localStorage.setItem('email', email)
+            localStorage.setItem('password', password)
+        })
+        .catch( apiResponse => {
+            console.log(apiResponse)
+        })
+    }
+    else{
+        console.log('No localStorage')
+    };
+};
+
 /* 
 Start interface
 */
     document.addEventListener('DOMContentLoaded', () => {
+        // Check user email/password
+        checkUser();
+
         // Get form register
         register(
             document.querySelector('#formRegister'),
             document.querySelector('#name').value,
             document.querySelector('#email').value,
             document.querySelector('#password').value
-        )
-    })
+        );
+
+        // Get form login
+        login(
+            document.querySelector('#formLogin'),
+            document.querySelector('#emailLogin').value,
+            document.querySelector('#passwordLogin').value
+        );
+    });
 //
